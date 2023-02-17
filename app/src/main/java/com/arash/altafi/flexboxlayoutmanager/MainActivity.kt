efcf5003d9659c70986e6757cc451ddc8dc43373
Arash Altafi <arashaltafi1377@gmail.com>
2023-02-17 11:23:53 +0330

    clean code

--- app/src/main/java/com/arash/altafi/flexboxlayoutmanager/MainActivity.kt
+++ app/src/main/java/com/arash/altafi/flexboxlayoutmanager/MainActivity.kt
@@ -2,34 +2,31 @@ package com.arash.altafi.flexboxlayoutmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import com.arash.altafi.flexboxlayoutmanager.databinding.ActivityMainBinding
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
-import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyUser : RecyclerView
-    private lateinit var btnShow : MaterialButton
-    private lateinit var btnClick : MaterialButton
-    private lateinit var adapter : AdapterUser
-    private var list : ArrayList<User> = ArrayList()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: AdapterUser
+    private var list: ArrayList<User> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         init()
     }
 
-    private fun init() {
-        findViews()
+    private fun init() = binding.apply {
         addUser()
         adapter = AdapterUser(list)
-        recyUser.adapter = adapter
-        recyUser.layoutManager = FlexboxLayoutManager(this).apply {
+        rcUser.adapter = adapter
+        rcUser.layoutManager = FlexboxLayoutManager(this@MainActivity).apply {
             flexDirection = FlexDirection.ROW
             justifyContent = JustifyContent.SPACE_AROUND
             alignItems = AlignItems.STRETCH
@@ -37,20 +34,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addUser() {
-        list.add(User(1 , "آرش الطافی 1",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 2",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 3",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 4",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-        list.add(User(1 , "آرش الطافی 5",R.drawable.arash))
-    }
-
-    private fun findViews() {
-        recyUser = findViewById(R.id.rc_user)
+        list.add(User(1, "arash 1", R.drawable.arash))
+        list.add(User(2, "arash 2", R.drawable.arash))
+        list.add(User(3, "arash 3", R.drawable.arash))
+        list.add(User(4, "arash 4", R.drawable.arash))
+        list.add(User(5, "arash 5", R.drawable.arash))
+        list.add(User(6, "arash 6", R.drawable.arash))
+        list.add(User(7, "arash 7", R.drawable.arash))
+        list.add(User(8, "arash 8", R.drawable.arash))
+        list.add(User(9, "arash 9", R.drawable.arash))
+        list.add(User(10, "arash 10", R.drawable.arash))
     }
 
 }
